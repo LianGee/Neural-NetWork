2f8286608fd65250660d60c952970919661b43a3
Lydoo <bo_bo0425@126.com>
2015-12-18 17:06:55 +0800

    nonlin function
    module nonlin

---
+++ 3LayerBp.py
@@ -0,0 +1,15 @@
+# Name:        3LayerBp
+# Purpose:
+#
+# Author:      LianGee
+#
+# Created:     15/12/2015
+# Copyright:   (c) LianGee 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def nonlin(x, deriv = False):
+    if (deriv == True):
+        return 1
+    else:
+        return 0
